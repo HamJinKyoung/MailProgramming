fd3aaae8685c1f2e38892e07ecdfaa8a13a750c3
HamJinkyoung <wlsrud3803@gmail.com>
2019-05-13 18:32:43 +0900

    5주차 solution

--- week05.java
+++ week05.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /*
 정수 배열과 타겟 숫자가 주어지면, 합이 타겟값이 되는 두 원소의 인덱스를 찾으시오.
 단, 시간복잡도 O(n) 여야 합니다.
@@ -10,9 +14,24 @@ Output: [0, 2] // 배열[0] + 배열[2] = 8
  */
 
 public class week05 {
+	
+	static void solution(int[] input, int target) {
+		Map map = new HashMap();
+		for (int i = 0; i < input.length; i++) {
+			int complement = target - input[i];
+			if (map.containsKey(complement)) {
+				System.out.println("[" + map.get(complement) + ", " + i + "]" );
+			}
+			map.put(input[i], i);
+		}
+	}
+
 
 	public static void main(String[] args) {
+		int[] input = {2,5,6,1,10};
+		int target = 8;
 		
+		solution(input, target);
 
 	}
 
