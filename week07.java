ae92ab1df69d13875da2a576aa243cd757ecb939
HamJinkyoung <wlsrud3803@gmail.com>
2019-11-26 16:55:53 +0900

    7주차 Mysolution

--- week07.java
+++ week07.java
@@ -9,10 +9,21 @@ Output: “cba 321 elppa”
  */
 
 public class week07 {
+	
+	static void Mysolution(String str) {
+		String[] words = str.split(" ");
+		String[] reversed = words;
+		for(int i=0; i<words.length; i++) {
+			reversed[i] = new StringBuilder(words[i]).reverse().toString()+" ";	// 맨 뒤에 공백..
+		}
+		for(int i=0; i<reversed.length; i++) {
+			System.out.print(reversed[i]);
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "abc 123 apple";
+		Mysolution(str);
 	}
 
 }
